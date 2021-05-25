1d3b557be1669ba5e0ce65093b355dc32fcee808
Daniel Espinosa <d111092.de@gmail.com>
2021-05-24 19:43:56 -0500

    tests: add tests over GifGridItem

---
+++ src/tests/components/GifGridItem.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import GifGridItem from '../../components/GifGridItem/GifGridItem';
+
+describe('GifGridItem tests', () => {
+    const url = 'url';
+    const title = 'title';
+    
+    const wrapper = shallow(<GifGridItem url={ url } title={ title }></GifGridItem>);
+    const img = wrapper.find('img');
+
+    test('should render without errors', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('div should have card style class', () => {
+        const div = wrapper.find('div').first()
+        expect(div.prop('className')).toBe('card');
+    });
+
+    test('img src should render properly', () => {
+        expect(img.prop('src')).toBe(url);
+    });
+
+    test('img alt should render properly', () => {
+        expect(img.prop('alt')).toBe(title);
+    });
+});
