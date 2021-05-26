77bd024acc14340feb85af628620d82f273d8bff
Daniel Espinosa <d111092.de@gmail.com>
2021-05-26 00:08:29 -0500

    tests: add submit & change simulation tests on AddCategoryField

--- src/tests/components/AddCategoryField.test.tsx
+++ src/tests/components/AddCategoryField.test.tsx
@@ -4,10 +4,34 @@ import AddCategoryField from '../../components/AddCategoryField/AddCategoryField
 
 describe('AddCategoryField tests', () => {
 
-    const setCategories = () => {};
+    const setCategories = jest.fn();
+    let wrapper = shallow(<AddCategoryField setCategories={ setCategories } />);
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        wrapper = shallow(<AddCategoryField setCategories={ setCategories } />);
+    });
 
     test('should render without errors', () => {
-        const wrapper = shallow(<AddCategoryField setCategories={ setCategories } />)
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should not send info on submit', () => {
+        wrapper.find('form').simulate('submit', {preventDefault: () => {}});
+
+        expect(setCategories).not.toHaveBeenCalled();
+    });
+
+    test('should simulate a succesful input attempt', () => {
+        // simulate onChange
+        const value = 'Hello World!';
+        const inputForm = wrapper.find('input');
+        inputForm.simulate('change', {target: {value}});
+
+        // simulate onSubmit
+        wrapper.find('form').simulate('submit', {preventDefault: () => {}});
+        expect(setCategories).toHaveBeenCalledTimes(1);
+
+        expect(inputForm.prop('value')).toBe('');
     });
 });
